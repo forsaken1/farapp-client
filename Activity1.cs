0d88165ab15588db377be6d3711ab90c6db89abd
showyoumuck <onl1ner.as@gmail.com>
2014-03-29 14:13:43 +1100

    Initial commit, added Gsm Client for push notiications

---
+++ Activity1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+using Gcm.Client;
+
+namespace FarApp
+{
+    [Activity(Label = "FarApp", MainLauncher = true, Icon = "@drawable/icon")]
+    public class Activity1 : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            const string id = "amazing-badge-534";
+            const string project_number = "1078115685642";
+
+            base.OnCreate(bundle);
+
+            GcmClient.CheckDevice(this);
+            GcmClient.CheckManifest(this);
+
+            SetContentView(Resource.Layout.Main);
+
+            Button button = FindViewById<Button>(Resource.Id.MyButton);
+
+            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+        }
+    }
+}
+
---
+++ GcmService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Gcm.Client;
+
+
+namespace FarApp
+{
+    [Service]
+    public class GcmService : GcmServiceBase
+    {
+        public GcmService() : base(GcmBroadcastReceiver.SENDER_IDS) { }
+
+        protected override void OnRegistered(Context context, string registrationId)
+        {
+            //Receive registration Id for sending GCM Push Notifications to
+        }
+
+        protected override void OnUnRegistered(Context context, string registrationId)
+        {
+            //Receive notice that the app no longer wants notifications
+        }
+
+        protected override void OnMessage(Context context, Intent intent)
+        {
+            //Push Notification arrived - print out the keys/values
+            if (intent == null || intent.Extras == null)
+                foreach (var key in intent.Extras.KeySet())
+                    Console.WriteLine("Key: {0}, Value: {1}");
+        }
+
+        protected override bool OnRecoverableError(Context context, string errorId)
+        {
+            return true;
+        }
+
+        protected override void OnError(Context context, string errorId)
+        {
+            //Some more serious error happened
+        }
+        void CreateNotification()
+        {
+            var notManager = GetSystemService(Context.NotificationService) as NotificationManager;
+            var uiIntent = new Intent(this, typeof(Activity1));
+            var notification = new Notification(Android.Resource.Drawable.SymActionEmail, "notif");
+            notification.Flags = NotificationFlags.AutoCancel;
+            notification.SetLatestEventInfo(this,"title","content",PendingIntent.GetActivity(this,0,uiIntent,PendingIntentFlags.CancelCurrent));
+            notManager.Notify(1, notification);
+        }
+
+    }
+}
